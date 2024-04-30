6230c09925d6c1a054fd3af387c9f68ac64ae1fe
Angela Tolwani <angela@tolwani.com>
2024-04-30 14:42:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    const matchingDrivers = drivers.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+    return matchingDrivers;
+};
+
+function fuzzyMatch(drivers, string) {
+    const matchingDrivers = drivers.filter(function (driver) {
+        const stringLength = string.length;
+        const partialDriver = driver.slice(0, stringLength)
+        return partialDriver === string;
+    });
+    return matchingDrivers;
+};
+
+function matchName(drivers, string) {
+    const matchingDrivers = drivers.filter(function(driver) {
+        return driver.name === string;
+    })
+    return matchingDrivers;
+};
